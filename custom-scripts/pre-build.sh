7e6c0d2f2496837fda5fac67ae988810bdf3d62b
GuilhermeHiago <guiga.santos1711@gmail.com>
2022-05-19 09:11:12 -0300

    adding debugger

--- custom-scripts/pre-build.sh
+++ custom-scripts/pre-build.sh
@@ -3,3 +3,4 @@
 make -C $BASE_DIR/../modules/simple_driver/
 cp $BASE_DIR/../custom-scripts/S41network-config $BASE_DIR/target/etc/init.d
 chmod +x $BASE_DIR/target/etc/init.d/S41network-config
+echo "tracefs /sys/kernel/tracing tracefs 0 0" >> output/target/etc/fstab
